9002a3ed186ec2fb2342d9f31bc624eba62bde55
chidi-1 <bev-olga@yandex.ru>
2021-01-28 21:47:48 +0300

    Фикс

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,12 +4,12 @@ const Layout = ({id, title, descr, urlBg, colorBg}) => {
     const descr_class = page_style.desc + " " + page_style.full;
     const styleBgColor = colorBg ? {background: `${colorBg}`} : {}
     const styleBgImg = urlBg ? {backgroundImage: `url(${urlBg})`} : {}
+    const styleTotal = Object.assign(styleBgColor, styleBgImg);
 
     return (
         <>
             <section
-                style={styleBgColor}
-                style={styleBgImg}
+                style={styleTotal}
                 className={page_style.root} id={id}
             >
                 <div className={page_style.wrapper}>
