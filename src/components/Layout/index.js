1ef48dcb8194536d7bb0cc496a2a6a9d288aeb9e
chidi-1 <bev-olga@yandex.ru>
2021-01-30 14:15:50 +0300

    Замечания

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,11 @@ const Layout = ({id, title, descr, urlBg, colorBg}) => {
     const descr_class = page_style.desc + " " + page_style.full;
     const styleBgColor = colorBg ? {background: `${colorBg}`} : {}
     const styleBgImg = urlBg ? {backgroundImage: `url(${urlBg})`} : {}
-    const styleTotal = Object.assign(styleBgColor, styleBgImg);
+    //const styleTotal = Object.assign(styleBgColor, styleBgImg);
+    const styleTotal = {
+        ...styleBgColor,
+        ...styleBgImg,
+    }
 
     return (
         <>
